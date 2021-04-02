0ba75d2be0b479acd4775d0a222a47e37db54919
SergeiOzhered <ser006serg@ukr.net>
2021-04-02 21:13:36 +0300

    added

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Project1
+# Sport Template
---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(Document).ready(function(){
+     $('mobile-button').on('click',function(){
+        $('.navigation').slideToggle();
+        $(this) ('opened');
+    
+    })
+
+});
+$(function() {
+  
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+    
+  });
