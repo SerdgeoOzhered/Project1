31fedeb1440ac6f74ad7cf2c37fdc3ccaa9b11bf
SergeiOzhered <ser006serg@ukr.net>
2021-04-13 13:41:29 +0300

    new file

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 $(Document).ready(function(){
-     $('mobile-button').on('click',function(){
-        $('.navigation').slideToggle();
-        $(this) ('opened');
-    
-    })
+    $('.mobile-button').on('click',function(){
+       $('.navigation').slideToggle();
+       $(this) ('opened');
+  //   console.log(yyyyyyyyy)
+     });
 
-});
-$(function() {
-  
-    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-      $(this)
-        .addClass('active').siblings().removeClass('active')
-        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-    });
     
-  });
+     $(function() {
+ 
+       $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+       $(this)
+           .addClass('active').siblings().removeClass('active')
+           .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+       });
+   });
+});
